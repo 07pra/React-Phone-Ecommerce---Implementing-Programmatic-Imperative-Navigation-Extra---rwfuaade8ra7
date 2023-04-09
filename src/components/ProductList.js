2b371747237a0bf5de2358308251a98095796455
Prashant kumar <120910332+07pra@users.noreply.github.com>
2023-04-09 11:47:37 +0530

    Update ProductList.js

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -11,10 +11,24 @@ export default class ProductList extends Component {
   render() {
     return (
       <React.Fragment>
-        START HACKING
+        <ProductWrapper className="py-5">
+          <div className="container">
+            <Title name="our" title="products" />
+            <div className="row">
+              <ProductConsumer>
+                {value => {
+                  return value.products.map(product => {
+                    return <Product key={product.id} product={product} />;
+                  });
+                }}
+              </ProductConsumer>
+            </div>
+          </div>
+        </ProductWrapper>
       </React.Fragment>
     );
   }
 }
 
 const ProductWrapper = styled.section``;
+Footer
